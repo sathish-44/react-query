a62c979fb1c11dc9b32c68f30a3cd3c70c576dbb
Sathish <sathishmcc94@gmail.com>
2022-01-12 13:56:35 +0530

    react-query commit

--- src/components/RQSuperHero.page.js
+++ src/components/RQSuperHero.page.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { useSuperHerosData } from '../hooks/useSuperHerosData'
+import { useSuperHerosData, useAddSuperHeroData } from '../hooks/useSuperHerosData'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export const RQSuperHeroPage = () => {
+    const [name, setName] = useState('')
+    const [alterEgo, setAlterEgo] = useState('')
 
     const onSuccess = (data) => {
         console.log('Perform side effect after data fetching', data)
@@ -11,6 +14,14 @@ export const RQSuperHeroPage = () => {
     const onError = (error) => {
         console.log('Perform side effect after encoutering error',error)
     }
+
+    const handleAddHeroClick = () => {
+        console.log("Handleclick", {name, alterEgo})
+        const hero = {name, alterEgo }
+        addHero(hero)
+    }
+
+    const { mutate : addHero, isLoading : addLoading, isError : addIsError, error : addErr} = useAddSuperHeroData()
 
     const { isLoading, data, isError, error, isFetching, refetch} = useSuperHerosData(onSuccess,onError)
 
@@ -24,7 +35,12 @@ export const RQSuperHeroPage = () => {
     return (
         <div>
             <h2>RQ Super Page</h2>
-            {/* <button onClick={refetch}>fecth hero</button> */}
+            <button onClick={refetch}>fecth hero</button>
+            <div>
+                <input type='text' value={name} onChange={e => setName(e.target.value)} />
+                <input type='text' value={alterEgo} onChange={e => setAlterEgo(e.target.value)} />
+                <button onClick={handleAddHeroClick}>Add Hero</button>
+            </div>
             {
                 data?.data.map(hero =>{
                     return <div key={hero.id}><Link to={`/rq-super-heroes/${hero.id}`}> {hero.name} </Link> </div>
--- src/hooks/useSuperHerosData.js
+++ src/hooks/useSuperHerosData.js
@@ -1,8 +1,8 @@
-import { useQuery } from 'react-query'
-import axios from 'axios'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import {request} from '../utils/axios.utils'
 
 const fecthSuperHeros = () => {
-    return axios.get('http://localhost:4000/superheros')
+    return request({ url : '/superheros'})
 }
 
 export const useSuperHerosData = (onSuccess,onError ) => {
@@ -16,4 +16,43 @@ export const useSuperHerosData = (onSuccess,onError ) => {
         // }
     }
     )
+}
+
+const addSuperHero = (hero) => {
+    console.log('hero', hero)
+    return request({ url : '/superheros', method : 'post', data: hero})
+}
+
+export const useAddSuperHeroData = () => {
+    const queryClient = useQueryClient()
+    return useMutation(addSuperHero, {
+        // onSuccess : (data) => {
+        //     // queryClient.invalidateQueries('super-heros')
+        //     queryClient.setQueryData('super-heros', (oldQueryData) => {
+        //         return {
+        //             ...oldQueryData,
+        //             data : [...oldQueryData.data, data.data]
+        //         }
+        //     })
+        // }
+        onMutate: async (newHero) => {
+          await queryClient.cancelQueries('super-heros')
+          const previousHero = queryClient.getQueryData('super-heros')
+          queryClient.setQueryData('super-heros', (oldQueryData) => {
+                    return {
+                        ...oldQueryData,
+                        data : [...oldQueryData.data, {id : oldQueryData?.data?.length + 1, ...newHero}]
+                    }
+                })
+                return {
+                    previousHero,
+                }
+        },
+        onError: (_error, _hero, context) => {
+            queryClient.setQueryData('super-heros', context.previousHero)
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries('super-heros')
+        }
+    })
 }
